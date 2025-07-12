8435a2dc1507198b300d0755ee84cc1c1c3ab74c
PutuMaichail <micfaruos@gmail.com>
2025-07-12 13:33:09 +0800

    .

--- belajar-rust/src/main.rs
+++ belajar-rust/src/main.rs
@@ -1,8 +1,7 @@
 fn main() {
-    let  result: i32 = 0;
-    println!("{:?}", result);
-}
-#[test]
-fn hello_test() {
-    main(5);
+    println!("Hello, world!");
+
+    println!("Hello, world!");
+    println!("Hello, world!");
+
 }
