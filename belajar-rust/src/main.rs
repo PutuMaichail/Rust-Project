f3bae23d55c859dba68d81bbb2a8a72ffcedcd21
PutuMaichail <micfaruos@gmail.com>
2025-07-12 14:01:51 +0800

    .

--- belajar-rust/src/main.rs
+++ belajar-rust/src/main.rs
@@ -10,3 +10,17 @@ fn main() {
 fn hello_test() {
     println!("This is a test function");
 }
+#[test]
+fn test_variabel() {
+    let name: &str = "putu riski wijaya";
+    println!("Hello, {}", name);
+}
+
+#[test]
+fn static_typing() {
+    let mut name: &str = "putu riski wijaya";
+    println!("Hello, {}", name);
+    
+    name = "Timoty Ronald";
+    println!("Hello, {}", name);
+}
